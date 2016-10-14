9341779e586aa70e07c471c73b927350bac6241d
Camille <Camille@10.0.0.115>
2016-10-13 19:20:05 -0500

    EasyShop Oct.13 commit1

---
+++ src/main/java/com/se452/easyshop/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.se452.easyshop.config;
+
+/**
+ *
+ * @author Camille
+ */
+public class WebSecurityConfig {
+    
+}
